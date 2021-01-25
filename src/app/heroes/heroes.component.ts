f770a6778b7cb90a788b7eecb69dfa83ab2bbba9
Nino Maruszewski <nino.maruszewski@gmail.com>
2021-01-25 12:07:42 -0600

    Added heroes list Tutorial #2 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
+import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -7,13 +8,15 @@ import { Hero } from './hero';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  selectedHero: Hero;
+  heroes = HEROES;
 
   constructor() {}
 
   // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
